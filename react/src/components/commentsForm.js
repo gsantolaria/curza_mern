2be1883298b46a8a74632503347ad7e1556db0ff
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-10-06 12:01:15 -0300

    create comments form

---
+++ react/src/components/commentsForm.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class CommentsForm extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div className='commentsForm'>
+                <form>
+                    <select id="" name="">
+                        <option value="">uno</option>
+                    </select>
+                </form>
+            </div>
+        )
+    }
+    
+}
+export default CommentsForm;
